6bdcbbcf070bb7fb06515f7fb22dddb6742cddac
vaddamanipallavi <vaddamanipallavi@gmail.com>
2025-07-31 12:26:39 +0530

    Fix: vite.svg import path for production build

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src="/vite.svg" alt="Vite Logo" />
+
+          
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
